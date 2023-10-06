339a1c6ebdfab839c36cb38dc96aae358d7c7af6
kryptocodes <srivatsantb@gmail.com>
2023-10-06 21:55:26 +0530

    init repo
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

---
+++ pages/user.tsx
@@ -0,0 +1,15 @@
+import React,{ useEffect, useState} from 'react'
+
+
+
+interface userProps {
+
+}
+
+const User: React.FC<userProps> = ({}) => {
+        return (<>
+
+        </>);
+}
+
+export default User
